efe222d0845554456d67fd1918bc18dc0de99496
Viljan Gerbrand Emanuel Van Raaij <vgraaij@stud.ntnu.no>
2024-01-30 01:09:04 +0100

    Update config

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,14 +1,56 @@
--- For removing annoying "No information available" when pressing K for docs
 return {
-  "folke/noice.nvim",
-  opts = function(_, opts)
-    table.insert(opts.routes, {
-      filter = {
-        event = "notify",
-        find = "No information available",
-      },
-      opts = { skip = true },
-    })
-    opts.presets.lsp_doc_border = true
-  end,
+  -- For removing annoying "No information available" when pressing K for docs
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+
+  -- Displays filename in the top right
+  {
+    "b0o/incline.nvim",
+    dependencies = { "folke/tokyonight.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("tokyonight.colors").setup()
+      require("incline").setup({
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.teal, guifg = colors.bg },
+            InclineNormalNC = { guifg = colors.fg, guibg = colors.bg_highlight },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+          return { { icon, guifg = color }, { " " }, { filename } }
+        end,
+      })
+    end,
+  },
+
+  -- Shows correspondig colors for hexcodes
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
 }
