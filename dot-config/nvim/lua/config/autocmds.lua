b2c17407e745217e84b42625f1cbb281d0a4b938
Viljan Gerbrand Emanuel Van Raaij <vgraaij@stud.ntnu.no>
2025-05-12 16:25:23 +0200

    Fix wrong nvim working dir

--- dot-config/nvim/lua/config/autocmds.lua
+++ dot-config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Ensures that right working directory is set when opening a directory in NeoVim.
+-- Previous behaviour: working directory in terminal would decide the working directory in neovim
+-- New behaviour: if you give a directory as an argument to neovim, it will use that as working directory
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    local dir = vim.fn.argv(0)
+    if dir ~= "" and vim.fn.isdirectory(dir) == 1 then
+      vim.cmd("cd " .. dir)
+    end
+  end,
+})
